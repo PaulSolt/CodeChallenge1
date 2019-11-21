8e033cf98dbe90d2a6871220175240a33909e8f7
Paul Solt <PaulSolt@gmail.com>
2019-11-21 07:05:50 -0500

    Refactored logic into a recursive algorithm. Created two test helper methods to facilitate testing in HackerRank environment. Created a compress method and tested 5 edge cases

--- CodeChallenge1/CodeChallenge1/main.swift
+++ CodeChallenge1/CodeChallenge1/main.swift
@@ -13,55 +13,114 @@
 
 import Foundation
 
-func makeWordNumber(input: String) -> [String] {
-    
-    // 1. Generate an array of letters replaced
-    
-    // iterate through each letter and replace with 1
-    
-    // walk through the string array
-    // track current index
-    // convert the current letter to a 1
-    
-    // ab
-    // 1b
-    
-    // TODO: bounds for zero length input
-    // guard let
-    
-    // word[index]  // replace character in subrange
-    var index = 0
-    var output: [String] = [input] // input, "1"]
-    
-    //let array = Array(input) // "a" "b" -> join
-    
-    for i in 0..<input.count  {
-        let range = NSRange(index...index) // +1
-        var word = NSString(string: input) // ab
-        
-        print(word)
-        var newWord = word.replacingCharacters(in: range, with: "1")
-        
-        output.append(String(newWord))
-        
-        // TODO: turn wor1 into [wo11, w1r1, 1or1]
-        
-        index += 1
+/*
+Objective: Given a word, generate all possible variations using these rules:
+1.    A letter can map to itself or 1
+For e.g. 'a' -> ['a', 1]
+
+2.    If 2 numbers are next to each other then add them up
+For e.g. for this 2 letter combination 'ab' -> ['ab', '1b', 'a1', '2']
+
+So the word 'word' would map to:
+'word' ->  ["word", "wor1", "wo1d", "wo2", "w1rd", "w1r1", "w2d", "w3", "1ord", "1or1", "1o1d", "1o2", "2rd", "2r1", "3d", "4"]
+*/
+
+func testEqual(expected: String, actual: String, message: String) {
+    if expected == actual {
+        print("Passed: \"\(message)\" test")
+        print("\tActual: \(actual)")
+    } else {
+        print("Failed: \"\(message)\" test\n\tExpected: \n\t\t\(expected)\n\tActual:\n\t\t\(actual))")
     }
-    
-    
-    
-    // 2. Squash the adjacent numbers
-    
-    return output
-    
 }
 
-let test1 = makeWordNumber(input: "a")
-print(test1) // ["a", "1"]
-assert(["a", "1"] == test1)
+func testEqualArray(expected: [String], actual: [String], message: String) {
+    if expected.sorted() == actual.sorted() {
+        print("Passed: \"\(message)\" test")
+        print("\tActual: \(actual)")
+    } else {
+        print("Failed: \"\(message)\" test\n\tExpected: \n\t\t\(expected.sorted())\n\tActual:\n\t\t\(actual.sorted())")
+    }
+}
 
-//let test2 = makeWordNumber(input: "ab")
-//print(test2) // ["ab", "1b", "a1", "2"]
+func generateWords(input: String) -> [String] {
+    let array = Array(input)
+    return chop(array: array)
+}
 
-//assert(["ab", "1b", "a1", "2"] == test2)
+func chop(array: [Character]) -> [String] {
+    guard let head = array.first else { return [] }
+    
+    let tail = Array(array.dropFirst()) // abc -> bc
+    
+    if tail.isEmpty {
+        return [String(head), "1"]
+    }
+
+    var mutations: [String] = []
+    let tree = chop(array: tail)  // [ bc, b1, 1c, 2] // 11
+    mutations.append(contentsOf: tree)
+    
+    print("head: \(head)")
+    print("M1: \(mutations)")
+    
+    var output: [String] = []
+    print("parent: \(String(array))")
+    for element in [String(head), "1"] {
+        for mutant in mutations {
+            let variation = String(element + mutant)
+            print("variation: \(variation)")
+            
+            // TODO: compress
+            output.append(variation)
+        }
+    }
+    
+    print("array: \(array)")
+    print("head: \(head)")
+    print("tail: \(tail)")
+    print("Mutations: \(mutations)")
+    print("output: \(output)")
+    print()
+    return output
+}
+
+
+func compress(string: String) -> String {
+    var output = ""
+    var counter = 0
+    for  letter in string {
+//        print("letter: \(letter)")
+        if letter.isNumber {
+            counter += 1
+        } else {
+            if counter != 0 {
+                output.append(String(counter))
+            }
+            output.append(letter)
+            counter = 0
+        }
+    }
+    if counter != 0 {
+        output.append(String(counter))
+    }
+    return output
+}
+let test4 = generateWords(input: "abc")
+testEqualArray(expected: ["abc", "ab1", "a1c", "1bc", "11c", "a11", "1b1", "111"], actual: test4, message: "abc")
+
+let testCompress1 = "111"
+testEqual(expected: "3", actual: compress(string: testCompress1), message: testCompress1)
+
+let testCompress2 = "a11"
+testEqual(expected: "a2", actual: compress(string: testCompress2), message: testCompress2)
+
+let testCompress3 = "11b"
+testEqual(expected: "2b", actual: compress(string: testCompress3), message: testCompress3)
+
+let testCompress4 = "1b1"
+testEqual(expected: "1b1", actual: compress(string: testCompress4), message: testCompress4)
+
+let testCompress5 = "abc"
+testEqual(expected: "abc", actual: compress(string: testCompress5), message: testCompress5)
+
